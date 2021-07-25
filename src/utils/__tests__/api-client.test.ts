1d56a406c368232d6f8b9682f1592bc420e9d0f0
huytrinh <dinhhuyams@gmail.com>
2021-07-25 10:44:56 +0300

    test: api-config

---
+++ src/utils/__tests__/api-client.test.ts
@@ -0,0 +1,80 @@
+import { client } from '../api-client'
+import { server, rest } from '../../test/server/test-server'
+import { RestRequest } from 'msw'
+
+beforeAll(() => server.listen())
+afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
+
+const apiURL = process.env.REACT_APP_API_URL
+
+test('make GET requests to the given endpoint', async () => {
+  const endpoint = 'test-endpoint'
+  const mockResult = { mockValue: 'VALUE' }
+
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      return res(ctx.json(mockResult))
+    })
+  )
+  const result = await client(endpoint)
+  expect(result).toEqual(mockResult)
+})
+
+test('add auth token when a token is provided', async () => {
+  const token = 'FAKE_TOKEN'
+  const endpoint = 'test-endpoint'
+  const mockResult = { mockValue: 'VALUE' }
+
+  let request: RestRequest | undefined
+  server.use(
+    rest.get(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      request = req
+      return res(ctx.json(mockResult))
+    })
+  )
+
+  await client(endpoint, { token })
+
+  expect(request?.headers.get('Authorization')).toEqual(`Bearer ${token}`)
+})
+
+test('allows for config overrides', async () => {
+  const endpoint = 'test-endpoint'
+  const mockResult = { mockValue: 'VALUE' }
+
+  let request: RestRequest | undefined
+  server.use(
+    rest.put(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      request = req
+      return res(ctx.json(mockResult))
+    })
+  )
+
+  const customConfig = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'fake-type' },
+  }
+
+  await client(endpoint, customConfig)
+
+  expect(request?.headers.get('Content-Type')).toEqual(
+    customConfig.headers['Content-Type']
+  )
+})
+
+test('when data is provided, it is stringified and the method default to POST', async () => {
+  const endpoint = 'test-endpoint'
+
+  server.use(
+    rest.post(`${apiURL}/${endpoint}`, async (req, res, ctx) => {
+      return res(ctx.json(req.body))
+    })
+  )
+
+  const data = { a: 'b' }
+
+  const result = await client(endpoint, { data })
+
+  expect(result).toEqual(data)
+})
