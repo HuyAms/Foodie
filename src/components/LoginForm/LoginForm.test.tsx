c8daa2bf6cf04a27cf1db36583ac031ba39692aa
huytrinh <dinhhuyams@gmail.com>
2021-07-26 23:03:20 +0300

    feat: add render util for wrapping test component

--- src/components/LoginForm/LoginForm.test.tsx
+++ src/components/LoginForm/LoginForm.test.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import {
+  userEvent,
+  screen,
+  render,
+  waitForLoadingToFinish,
+} from '../../test/app-test-utils'
 import LoginForm from './index'
 import { buildLoginForm } from '../../test/generate'
-import userEvent from '@testing-library/user-event'
-import { waitForLoadingToFinish } from '../../test/app-test-utils'
-import * as faker from 'faker'
 
 test('submitting the form calls onSubmit with username and password', async () => {
   const handleSubmit = jest.fn(() => Promise.resolve())
 
-  render(
+  await render(
     <LoginForm submitButton={<button>Submit</button>} onSubmit={handleSubmit} />
   )
 
@@ -34,7 +36,7 @@ test('show an error message when fails to login', async () => {
     Promise.reject(Error('There is an error while trying to login'))
   )
 
-  render(
+  await render(
     <LoginForm submitButton={<button>Submit</button>} onSubmit={handleSubmit} />
   )
 
@@ -47,9 +49,6 @@ test('show an error message when fails to login', async () => {
   userEvent.type(passwordInput, password)
 
   userEvent.click(screen.getByRole('button', { name: /submit/i }))
-
-  expect(handleSubmit).toHaveBeenCalledWith(email, password)
-  expect(handleSubmit).toHaveBeenCalledTimes(1)
 
   await waitForLoadingToFinish()
 
